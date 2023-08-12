dc33d9f1e50a0937dc76a9e0fb01c7b89af0effc
M-Ibrahim96 <mohamed.ibrahim.abdelhamid96@gmail.com>
2023-08-12 01:27:17 -0700

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints sentnce followed by a new line, to the standard error.
+ * without using putchar - puts - printf
+ *
+ * Return: Alwayes 1
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	return (1);
+}
