889abf9147d7cea7ec3ff13b2f8e1f10e925ecc9
M-Ibrahim96 <mohamed.ibrahim.abdelhamid96@gmail.com>
2023-08-16 12:02:23 -0700

    9 table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,11 +18,11 @@ void times_table(void)
 
 			mult = n1 * n2;
 
-			if (multi <= 9)
+			if (mult <= 9)
 				_putchar(' ');
 			else
-				_putchar((multi / 10) + '0');
-			_putchar((multi % 10) + '0');
+				_putchar((mult / 10) + '0');
+			_putchar((mult % 10) + '0');
 		}
 		_putchar('\n');
 	}
