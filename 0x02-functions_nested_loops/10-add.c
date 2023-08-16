c61291efe6f5414bd171b630f6cdbc401e3e06fa
M-Ibrahim96 <mohamed.ibrahim.abdelhamid96@gmail.com>
2023-08-16 12:07:36 -0700

    added

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - function that add 2 digits
+ * add is function that takes 2 digit and return sum of them
+ * @n1 : First number
+ * @n2 : Second number
+ * Return: Summion of numbers
+ */
+int add(int n1, int n2)
+{
+	int n1, n2;
+
+	return (n1 + n2);
+}
