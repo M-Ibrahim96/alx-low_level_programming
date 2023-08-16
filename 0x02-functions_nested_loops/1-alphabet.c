aeaa178bc9e38eb121405bfa718ef37d8690d022
M-Ibrahim96 <mohamed.ibrahim.abdelhamid96@gmail.com>
2023-08-16 06:16:21 -0700

    Program print from a - z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase from a - z
+ * Descpritopn: prints from a to z in lowercase
+ * without return
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = "a"; c <= "z"; c++)
+		_putchar(c);
+	_putchar("\n");
+}
