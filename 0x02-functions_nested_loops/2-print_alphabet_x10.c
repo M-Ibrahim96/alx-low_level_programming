cc2ab7a1ecc0276b8ed4e6fc8c11f6d350bddc69
M-Ibrahim96 <mohamed.ibrahim.abdelhamid96@gmail.com>
2023-08-16 06:25:45 -0700

    Prints 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * void print_alphabet_x10(void) - prints alphabet 10 times
+ * Description: print all letters 10 times
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+	int i = 1;
+
+	for (i = 1; i <= 10; i++)
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+}
