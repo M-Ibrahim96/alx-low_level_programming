fe5549f5bc82b168ee4da1400eaad5a96dc5bb18
M-Ibrahim96 <mohamed.ibrahim.abdelhamid96@gmail.com>
2023-08-18 18:54:57 -0700

    if

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print 0123456789
+ *
+ * Return: always 0
+ */
+void print_most_numbers(void)
+{
+	int c = 0;
+
+	while (c <= 9)
+	{
+		if ((c + 48 == '2' || c + 48 == '4')
+				continue;
+		else
+		_putchar(c + 48);
+		c++;
+	}
+	_putchar('\n');
+}
