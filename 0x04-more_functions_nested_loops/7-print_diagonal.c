2a6beb9860eb883916031d635c52123767b94686
M-Ibrahim96 <mohamed.ibrahim.abdelhamid96@gmail.com>
2023-08-18 19:46:04 -0700

    7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function to print
+ * @n is number of lines
+ * Return: Always 0
+ */
+void print_diagonal(int n)
+{
+	int num, car;
+
+	if (n <= 0)
+		_putchar('\n');
+	for (num = 0; num <= n; num++)
+	{
+		for (car = 0; car < num; car++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
