2797824c47f51a8d7dbad25e42bf89622c303ec0
M-Ibrahim96 <mohamed.ibrahim.abdelhamid96@gmail.com>
2023-08-21 17:39:08 -0700

    added

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - function print a string followed by a newline
+ * @str: string paratmeters to print
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + '0');
+		str**;
+	}
+	_putchar('\n');
+}
