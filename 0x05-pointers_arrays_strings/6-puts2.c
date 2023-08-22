1322fd00a75bf93ed7dcd36183048fc952d0b2d3
M-Ibrahim96 <mohamed.ibrahim.abdelhamid96@gmail.com>
2023-08-21 18:16:41 -0700

    push

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - function prints every character of string
+ * @str: string parameter input
+ *
+ * Return: nothing.
+ */
+void puts2(char *str)
+{
+	int o;
+
+	for (o = 0; str[o] != '\0'; o++)
+	{
+		if (o % 2 == 0)
+			_putchar(str[o]);
+	}
+	_putchar('\n');
