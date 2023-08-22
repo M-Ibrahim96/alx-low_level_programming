99474b4333de2bdaf121bcb4d2255a12d0d29f25
M-Ibrahim96 <mohamed.ibrahim.abdelhamid96@gmail.com>
2023-08-21 18:37:05 -0700

    {{{

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string into an integer.
+ *
+ * @s: the string to use.
+ *
+ * Return: integer.
+ */
+
+int _atoi(char *s)
+{
+	unsigned int n = 0;
+	int sign = 1;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			n = (n * 10) + (*s - '0');
+		else if (n > 0)
+			break;
+	} while (*s++);
+
+	return (num * sign);
+}
