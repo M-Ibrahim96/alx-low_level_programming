ee0d551088bdca11441c44738efaa2c747269cf9
M-Ibrahim96 <mohamed.ibrahim.abdelhamid96@gmail.com>
2023-08-26 21:40:31 -0700

    {PO

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,15 +10,14 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int index = strlen(dest);
-	int a = 0;
+	int c, i;
 
-	while (a < n && *src)
-	{
-		dest[index + a] = *src;
-		src++;
-		a++;
-	}
-	dest[index + a] = '\0';
+	c = 0;
+
+	while (dest[c])
+		c++;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[c + i] = src[i];
+	dest[c + i] = '\0';
 	return (dest);
 }
