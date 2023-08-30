6d6562d26cf88c6fc70bce72b963afe2b71c5a7c
M-Ibrahim96 <mohamed.ibrahim.abdelhamid96@gmail.com>
2023-08-29 22:36:08 -0700

    4 - 5

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -8,9 +8,17 @@
  */
 int _pow_recursion(int x, int y)
 {
-	if ((y == -1) || (y == 0))
+	if (y < 0)
 	{
 		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else if (y == 1)
+	{
+		return (x);
 	}
 	return (x * _pow_recursion(x, y - 1));
 }
---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - return the sqrt root
+ * @n: integer to be enterd
+ * Return: sqrt of number
+ */
+int _sqrt_recursion(int n)
+{
+	return (square(n, 1));
+}
+/**
+ * square - function to get square root
+ * @n: intger to search for root
+ * @val: Square root
+ * Return: Sqrt of number
+ */
+int square(int n, int val)
+{
+	if (val * val == n)
+	{
+		return (val);
+	}
+	else if (val * val < n)
+	{
+		return (square(n, val + 1));
+	}
+	else
+		return (-1);
+}
