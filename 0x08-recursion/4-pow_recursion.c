1bea98ab095415cdd65fff7c66cd11f39c621886
M-Ibrahim96 <mohamed.ibrahim.abdelhamid96@gmail.com>
2023-08-29 22:20:24 -0700

    POMA

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - fucntion value of x raised to y
+ * @x: integer to be raised
+ * @y: inter is the power of x
+ * Return: x to power of y.
+ */
+int _pow_recursion(int x, int y)
+{
+	if ((y == -1) || (y == 0))
+	{
+		return (-1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
