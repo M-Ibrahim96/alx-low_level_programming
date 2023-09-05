f06608a1e8f34a155e108fc50a5f6fcc836e3acf
M-Ibrahim96 <mohamed.ibrahim.abdelhamid96@gmail.com>
2023-09-05 16:39:36 -0700

    Added stdlib

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * *create_array - creates a new array.
@@ -9,7 +10,7 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *n = malloc(sizeof(c) * size);
+	char *n = malloc(size);
 
 	if (size == 0 || n == 0)
 		return (0);
