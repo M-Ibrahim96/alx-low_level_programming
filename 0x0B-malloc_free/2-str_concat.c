ccd66bc9dd1ea4c6ff555d7e481d8049d2519592
M-Ibrahim96 <mohamed.ibrahim.abdelhamid96@gmail.com>
2023-09-05 17:41:04 -0700

    REPLACED

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -26,9 +26,9 @@ char *str_concat(char *s1, char *s2)
 	char *m;
 
 	if (s1 == NULL)
-		s1 = '\0';
+		s1 = "\0";
 	if (s2 == NULL)
-		s2 = '\0';
+		s2 = "\0";
 
 	size1 = _slen(s1);
 	size2 = _slen(s2);
