d3d3f9f6d19d6cb94d8ace43c1a06122a9a63566
M-Ibrahim96 <mohamed.ibrahim.abdelhamid96@gmail.com>
2023-09-08 00:16:42 -0700

    Cheated

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,44 +3,52 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - calculate and return string length
+ * @string: string
+ * Return: string length
+ */
+int _strlen(char *string)
+{
+	int i;
+
+	for (i = 0; string[i] != '\0'; i++)
+		;
+	return (i);
+}
+/**
  * string_nconcat - concatenate s1 and n bytes of s2; return ptr to string
- * @s1: pointer to the first string
- * @s2: pointer th the second string
- * @n: number of bytes from s2 to concat
- * Return: Pointer to space in memory containg concatenated string
+ * @s1: string 1
+ * @s2: string 2
+ * @n: n bytes to concat from string 2
+ * Return: pointer to concatenated string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *str;
-	unsigned int i, j, s1_length, s2_length;
+	char *ptr;
+	int num, len, i, j;
 
-	if (s1 == NULL)
+	num = n;
+
+	if (s1 == NULL) /* account for NULL strings */
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
-	for (s1_length = 0; s1[s1_length] != '\0'; s1_length++)
-		;
-	for (s2_length = 0; s2[s2_length] != '\0'; s2_length++)
-		;
-	if (n < 0)
+	if (num < 0) /* account for negative n bytes */
 		return (NULL);
-	if (n >= s2_length)
-		n = s2_length;
+	if (num >= _strlen(s2)) /* account for n too big */
+		num = _strlen(s2);
 
-	str = malloc(s1_length + n + 1);
+	len = _strlen(s1) + num + 1; /* +1 to account for null pointer */
 
-	if (str == NULL)
-	{
+	ptr = malloc(sizeof(*ptr) * len); /* malloc and check for error */
+	if (ptr == NULL)
 		return (NULL);
-	}
-	for (i = 0; s1[i] != '\0'; i++)
-		str[i] = s1[i];
-	for (j = 0; j < n; j++)
-	{
-		str[i] = s2[j];
-		i++
-	}
-	str[i] = '\0';
-	return (str);
+
+	for (i = 0; s1[i] != '\0'; i++) /* concat */
+		ptr[i] = s1[i];
+	for (j = 0; j < num; j++)
+		ptr[i + j] = s2[j];
+	ptr[i + j] = '\0';
+
+	return (ptr);
 }
